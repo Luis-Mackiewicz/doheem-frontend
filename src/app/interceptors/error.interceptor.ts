b31d86cfa2955792cf781d62c7f7acb15b93d0dd
Luis Miguel Mackiewicz <luismiguelpix@gmail.com>
2026-06-16 19:01:29 -0300

    feat: adicionar refresh automático de token ao receber 401

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
@@ -12,8 +12,20 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
-        auth.clearSession();
-        router.navigate(['/login']);
+        return auth.refresh().pipe(
+          switchMap(res => {
+            auth.setSession(res);
+            const cloned = req.clone({
+              setHeaders: { Authorization: `Bearer ${res.token}` },
+            });
+            return next(cloned);
+          }),
+          catchError(() => {
+            auth.clearSession();
+            router.navigate(['/login']);
+            return throwError(() => error);
+          }),
+        );
       }
       return throwError(() => error);
     }),
